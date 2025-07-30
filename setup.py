184be6551c252cfda3a87ca83892d3e5d6a657aa
Sonam-22 <sonampriya7070@gmail.com>
2025-07-30 12:22:10 +0200

    Fix setup

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='go2-webrtc-connect',
     version='1.0.0',
     author='legion1581',
     author_email='legion1581@gmail.com',
-    packages=find_packages(),
+    packages=find_packages(),      
     install_requires=[
+        f"aioice @ file://{os.path.join(here, 'libs/aioice')}",
         'aiortc==1.9.0', 
         'pycryptodome',
         'opencv-python',
