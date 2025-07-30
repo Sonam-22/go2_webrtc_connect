107b994af5c08fb7e571905038ba308adea13e8d
Sonam-22 <sonampriya7070@gmail.com>
2025-07-30 10:55:34 +0200

    change to error message

--- examples/validations/vaildate-aiortc.py
+++ examples/validations/vaildate-aiortc.py
@@ -4,4 +4,4 @@ logging.info("Checking aiortc installation")
 a = Connection(ice_controlling=False)
 b = Connection(ice_controlling=False)
 if a.local_username != b.local_username:
-  logging.info("aoice installation error.")
+  logging.error("aoice installation error.")
